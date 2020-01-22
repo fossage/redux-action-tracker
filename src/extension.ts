719b97a3f198953b2d456d911fac95bbf6163cdb
Justin Foss <jfoss124@gmail.com>
2020-01-22 08:45:05 -0800

    Misc code cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -16,42 +16,42 @@ interface ActionCreators {
 	};
 }
 
-const findGlob = '**/action-creators{**/*, *}.js';
-const usagesGlob = '**/store/**/*.js';
+const actionCreatorDefinitionsGlob = '**/action-creators{**/*, *}.js';
+const actionConsumersGlob = '**/store/**/*.js';
 
 const functionDeclarationRE = /(?<=\bfunction\s+)\w+/;
 const actionTypeRE = /\s+(?<=return\s+\{[\w\s]+type:\s)[A-Z_]+/;
 const createActionCreatorRE = /createActionCreator\(/;
 const createActionCreatorTypeRE = /(?<=createActionCreator\(\s*)\w+/;
 const createActionCreatorNameRE = /(?<=const\s)\w+(?=\s=\screateActionCreator\(\s*\w+)/;
+const usageFileNameFilterRE = /(action-creators|-tests|types)(\/[\w-]+)*\.js$/;
 
 let actionCreators: ActionCreators = {};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	const files = await vscode.workspace.findFiles(findGlob);
-	let usageFiles = await vscode.workspace.findFiles(usagesGlob);
+	const actionCreatorDefinitionFiles = await vscode.workspace.findFiles(actionCreatorDefinitionsGlob);
+	let actionConsumerFiles = await vscode.workspace.findFiles(actionConsumersGlob);
 
-	if(!files.length || !usageFiles.length) {
+	if(!actionCreatorDefinitionFiles.length || !actionConsumerFiles.length) {
 		return;
 	}
 	
-  usageFiles = usageFiles.filter(file => !/(action-creators|-tests|types)(\/\w+)*\.js$/.test(file.path));
+  actionConsumerFiles = actionConsumerFiles.filter(file => !usageFileNameFilterRE.test(file.path));
 
-	for(const fileDescriptor of files) {
-		const doc = await vscode.workspace.openTextDocument(fileDescriptor.path);
-		const text = doc.getText();
-		
-		let cursor = -1;
-    let position = new vscode.Position(0,0);
-		let line = doc.lineAt(position);
+	for(const fileDescriptor of actionCreatorDefinitionFiles) {
+		const definitionDocument = await vscode.workspace.openTextDocument(fileDescriptor.path);
 
-		while(line.lineNumber < doc.lineCount - 1) {
-			let type;
+    let currentPosition = new vscode.Position(0,0);
+		let line = definitionDocument.lineAt(currentPosition);
+		const endPosition = new vscode.Position(definitionDocument.lineCount + 1, 0);
+
+		while(line.lineNumber < definitionDocument.lineCount - 1) {
+			let actionType;
 			let actionCreatorName;
 
-			const nextText = text.slice(cursor);
+			const nextText = definitionDocument.getText(new vscode.Range(currentPosition, endPosition));
 			const functionDeclarationMatches = functionDeclarationRE.exec(line.text);
 			const createActionCreatorsMatches = createActionCreatorRE.test(line.text);
 
@@ -59,56 +59,56 @@ export async function activate(context: vscode.ExtensionContext) {
 				const typeMatch = nextText.match(actionTypeRE);
 	
 				if(typeMatch?.length) {
-					type = typeMatch[0].trim();
+					actionType = typeMatch[0].trim();
 				  actionCreatorName = functionDeclarationMatches[0].trim();
 				}	
 			} else if(createActionCreatorsMatches) {
 				const typeMatch = nextText.match(createActionCreatorTypeRE);
 
         if(typeMatch?.length) {
-					type = typeMatch[0].trim();
+					actionType = typeMatch[0].trim();
 					actionCreatorName = nextText.match(createActionCreatorNameRE)?.[0]?.trim();
 				}
 			}
 
-      if(type && actionCreatorName) {
-				for(const usageFileDescriptor of usageFiles) {
-					const doc = await vscode.workspace.openTextDocument(usageFileDescriptor.path);
-					const text = doc.getText();
+      if(actionType && actionCreatorName) {
+				for(const actionConsumerFileDescriptor of actionConsumerFiles) {
+					const actionConsumerDocument = await vscode.workspace.openTextDocument(actionConsumerFileDescriptor.path);
+					const actionConsumerFileText = actionConsumerDocument.getText();
 
-					let position = text.indexOf(type);
+					let cursor = actionConsumerFileText.indexOf(actionType);
 
-					while(position !== -1) {
+					while(cursor !== -1) {
 						if(!actionCreators[actionCreatorName]) {
 							actionCreators[actionCreatorName] = { 
-								type, 
+								type: actionType, 
 								usages: [], 
 								usagesPerUri: {} 
 							};
 						}
 
-						const pos = doc.positionAt(position);
-						const uri = usageFileDescriptor.path;
+						const uri = actionConsumerFileDescriptor.path;
+						const documentPosition = actionConsumerDocument.positionAt(cursor);
 						const { usagesPerUri } = actionCreators[actionCreatorName];
-						usagesPerUri[uri] = usagesPerUri[uri] ? usagesPerUri[uri] + 1 : 1;
+
+						usagesPerUri[uri] = (usagesPerUri[uri] || 0) + 1;
 
 						// skip the first usage as we assume that is the import of the action type
 						if(usagesPerUri[uri] > 1) {
 							actionCreators[actionCreatorName].usages.push({
 								uri, 
-								line: pos.line, 
-								character: pos.character
+								line: documentPosition.line, 
+								character: documentPosition.character
 							});
 						}
 
-						position = text.indexOf(type, position + type.length);
+						cursor = actionConsumerFileText.indexOf(actionType, cursor + actionType.length);
 					}
 				}
 			}
-			
-			cursor += line.text.length + 1;
-			position = position.translate(1);
-			line = doc.lineAt(position);
+
+			currentPosition = currentPosition.translate(1);
+			line = definitionDocument.lineAt(currentPosition);
 		}
 	}
 
